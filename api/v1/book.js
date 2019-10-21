667f0d3f503f27afb3da21c6ff629b0a64e8b22a
cwzp990 <cwzp990@gmail.com>
2019-10-21 15:10:23 +0800

    异常处理

--- api/v1/book.js
+++ api/v1/book.js
@@ -1,10 +1,11 @@
 const Router = require('koa-router')
 const router = new Router()
 
-router.get('/v1/book', ctx => {
-  ctx.body = {
-    key: '书籍'
-  }
+router.post('/v1/:id/book', ctx => {
+  const params = ctx.params
+  const query = ctx.request.query
+  const headers = ctx.request.headers
+  const body = ctx.request.body
 })
 
 module.exports = router
